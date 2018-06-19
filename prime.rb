e1a9843ef3e4ba6652908513e6e3c560d5425c82
Jesse McReady <jessemcready@gmail.com>
2018-06-19 20:47:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(number)
-  if number < 1
+  if number <= 1
     return false
   end
-  if number == 1 || number == 2 
+  if number == 2 
     return true
   end
   i = 2...number
